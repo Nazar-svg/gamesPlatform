4debf999a45785dacd14a8cf1a5972abad92f3d8
Nazar-svg <nazar.kolos90@gmail.com>
2020-09-11 12:20:05 +0300

    create menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import logo from './imagen/joystick.svg';
+import Button from './Components/UI/Button';
 import axios from 'axios';
 
 class App extends Component {
   state = {
-    gamesPlatforms: []
+    gamesPlatforms: [],
+    showMenu: false
   }
 
   componentDidMount() {
@@ -17,15 +19,18 @@ class App extends Component {
       }
     })
       .then(response => {
-        console.log(this.state.gamesPlatforms)
         this.setState({
           gamesPlatforms: response.data.results
         })
-        console.log(this.state.gamesPlatforms)
       })
       .catch(err => {
         console.log(err);
       });
+  }
+  onFilterHandler = () => {
+    this.setState({
+      showMenu: !this.state.showMenu
+    })
   }
 
   render() {
@@ -35,8 +40,29 @@ class App extends Component {
           <div className="Logo">
             <img src={logo} alt="logo" />
           </div>
+          <Button
+            type="primary"
+            onFilterHandler={this.onFilterHandler}
+          >
+            Filter
+          </Button>
+
+          {this.state.showMenu
+            ?
+            <div className="filterMenu">
+              <h2>Game Platforms</h2>
+              <input
+                    type="text"
+                    className="form-control"
+                    placeholder="platform"
+                    id="platform"
+                />
+            </div>
+            : null
+          }
+
         </header>
-        <div className="Container" >
+        <div className="Continer" >
           <div className="inner">
             {this.state.gamesPlatforms.map((item, id) => {
               return (
@@ -53,6 +79,7 @@ class App extends Component {
             })}
           </div>
         </div>
+
       </div>
     );
   }
